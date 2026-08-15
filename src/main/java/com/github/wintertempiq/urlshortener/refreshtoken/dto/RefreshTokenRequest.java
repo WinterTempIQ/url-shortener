3c733a382dda647537851aa937dda590c7eec17b
WinterTempIQ <ArtChern14@gmail.com>
2026-08-15 13:23:32 +0300

    refactor: simplify DTO Lombok annotations

--- src/main/java/com/github/wintertempiq/urlshortener/refreshtoken/dto/RefreshTokenRequest.java
+++ src/main/java/com/github/wintertempiq/urlshortener/refreshtoken/dto/RefreshTokenRequest.java
@@ -2,7 +2,6 @@ package com.github.wintertempiq.urlshortener.refreshtoken.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class RefreshTokenRequest {
--- src/main/java/com/github/wintertempiq/urlshortener/user/dto/LoginRequestDto.java
+++ src/main/java/com/github/wintertempiq/urlshortener/user/dto/LoginRequestDto.java
@@ -3,7 +3,6 @@ package com.github.wintertempiq.urlshortener.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class LoginRequestDto {
